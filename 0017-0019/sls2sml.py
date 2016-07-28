4ceb5a33593fd3c7552956ab259a0c2dc62d12ad
acceml <1023598954@qq.com>
2016-07-28 20:43:52 +0800

    complete basic practice of py

--- 0017-0019/sls2sml.py
+++ 0017-0019/sls2sml.py
@@ -2,4 +2,10 @@
 # -*- encoding: utf-8 -*-
 # Date: 2016/7/27
 # Author: Acceml
+
 if __name__ == '__main__':
+    l = []
+    with open('filtered_words.txt', 'r') as f:
+        for each_line in f:
+            l.append(each_line.split('\n'))
+    print(l)
